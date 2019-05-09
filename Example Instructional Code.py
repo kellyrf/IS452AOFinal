b4ed2e02388540fdf38563bd3c4dc1e153da88c2
kellyrf <50420081+kellyrf@users.noreply.github.com>
2019-05-09 13:28:26 -0400

    Update Example Instructional Code.py

--- Example Instructional Code.py
+++ Example Instructional Code.py
@@ -1,6 +1,14 @@
 # Open workshop with this video: https://www.youtube.com/watch?v=o0u4M6vppCI 
 
-# Before you begin:
+# # Before you begin:
+# - review this content carefully and make any necessary modifications for yourself and your instructional style.
+# - this is meant to be a guide to the workshop, but not an exact script. Take liberties with the story you create, and allow 
+#   for student input to guide the workshop, since it is created for their benefit.
+# - The code in this is chunked purposefully to be run together. Be sure to comment out anything you don't want to deal with
+#   as the lesson progresses.
+# - Remember to note throughout that syntax and semantics matter, keep your code as simple as possible (with one idea per line),
+#   and errors are your friend.
+
 
 
 ######################## Openning the Shell ########################
@@ -23,34 +31,41 @@
 
 ######################## Printing a String ########################
 
-# In order to see the results of a program, you often need to print your output.
-# This can be achieved by:
-
+# In order to see the results of any program, you often need to print your output. Click inside of the type box loaded with the new 
+# program.
+# Achieve your first line of code with:
 print("Hello World")
 
+# Run the program with the small green triangle. 
+
 # In this statement, the function 'print()' is passing the literal string "Hello World".
-# When you run the program, you will get "Hello World"
+# When you run the program, you will get 
+"Hello World"
+
 # This also works with single quotes (' ') and triple quotes (""" """), when you print an item as so: 
 print ('Hello World')
 print ("""Hello 
 World""")
 
-# Single quotes are helpful if you need to print an item with quotations in it, and triple quotes are helpful
-# if you need to print an item that spans multiple lines.
+# Single quotes are helpful if you need to print an item with quotations in it, like:
+print ('Hello, do you need to "emphasize" this word?')
+
+# and triple quotes are helpful if you need to print an item that spans multiple lines, and you want to maintain the new lines:
+print ("""Alexander Hamilton
+My name is Alexander Hamilton
+And there's a million things I haven't done
+But just you wait, just you wait""")
 
 # You can use print(<parameter>) to print a number of things for a program.
 # In programming, a sentence that you would see printed out is considered a string, which is a fancy
 # way of saying a line of characters.
 # They don't necessarily have to be words, they can be any character you can type out on a keyboard.
-# However, whatever you print in the quotes, is exactly what you get out of the quotes.
-
-#### Example ####
-
+# However, whatever you print in the quotes, is exactly what you get out of the quotes. Examples:
 print("Gibb3rish")
 print("3+2")
 print('you're walking in the woods...&&%$#')
 
-# the last print statement doesn't work - can you tell me why?
+# the last print statement doesn't work, and we got an error - can you tell me why?
 print("you're walking in the woods...&&%$#")
 
 
@@ -60,19 +75,16 @@ print("you're walking in the woods...&&%$#")
 # When you have a string, sometimes you need to give it a name to call it back, as you might need it later.
 # To do this, you can make the string a variable. Variables are like in math - the variable x represents
 # some sort of number in math, and you can make x equal a specific number to get a result you want.
-
 x = 3
 print(x)
 
 # You can do the same with a string. If you have a super long string (or short string you need >1), you can make it equal a variable.
-
 inthewoods = """You’re walking in the woods, there is no one around, and your phone is dead. Out of the corner of your eye 
 you spot him. Its:"""
 
 # As you can see, inthewoods is a pretty long string that takes multiple lines, and you don't want to type that out every time
 # you want to have it print. 
 # So, to avoid that struggle, we set it equal to the name 'inthewoods' and pass that through the print() function.
-
 print(inthewoods)
 
 # Notice that it prints the content of inthewoods, but not the term "inthewoods". 
@@ -91,14 +103,13 @@ inthewoods = """You’re walking in the woods, there is no one around, and your 
 you spot him. Its:"""
 choiceA = "A. Shia Labeouf"
 choiceB = "B. An adorable cat"
-# 
+
 # When you make a program, you may want information from the person who is using the program.
 # For our purposes, we want to get the user prompts to know what kind of story they want.
 # In a choose your own adventure story, you have options to make the story yours. 
 # Ours won't be as complex as the memorable books from childhood or Black Mirror: Bandersnatch, but will give simple prompts to choose
 # a different story for each choice.
 # To do this, we need to prompt for an input, where the user interacts with the program by inputing content.
-
 print(inthewoods)
 print(choiceA)
 print(choiceB)
@@ -127,18 +138,18 @@ B. An adorable cat"""
 print(inthewoods)     
 answer0 = (input("Your choice (a/b): ")      
       
-# Now, we have somewhere to start the story.      
-# Break for lunch and questions      
+# Now, what do we do with that input? We have somewhere to start the story.      
+# # Break for lunch and questions      
 
 	   
 	   
 ######################## If/Else Statements ########################
+
 # You may have talked about in math using if statements. 
 # Whether you did or did not doesn't provide much advantage, so no worries to those who didn't.
-# An if statement provides a way of making choices in a program, where whatever input is received may then lead the program to make 
+# An if statement provides a way of having options in a program, where whatever input is received may then lead the program to make 
 # certain choices so as to continue in a specific direction. 
 # So let's see our story continue:
-
 Meow1 = """The adorable cat walks up to you and sits on a large leaf. The cat gives you an inquisitive look, and meows. You:
 A. kneel and reach out cautiously for the cat to sniff you
 B. step back, because you are intensely allergic to cats""" 
@@ -149,7 +160,11 @@ around. You:
 A. Run for your life
 B. Decide to fight back"""
 
-if answer0.lower().strip() == a:
+print(inthewoods)     
+answer0 = (input("Your choice (a/b): ")  
+	   
+if answer0.lower().strip() == a: 
+# here, you are starting with the if statement - if the answer by the user is an a, being if answer0 == a, then the following occurs:
 	   print(Shia1)
 	   answershia1 = input("What do you do? (a/b): ")
 elif answer0.lower().strip() == b:
