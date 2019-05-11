96acd1be1bb3e4a70eeab3098f5c272e4148a3de
kellyrf <50420081+kellyrf@users.noreply.github.com>
2019-05-11 16:05:01 -0400

    Update Example Instructional Code.py

--- Example Instructional Code.py
+++ Example Instructional Code.py
@@ -1,9 +1,14 @@
-# these are comments
+# To start, these are comments! Be sure to explain that "hashtags"/pound key is used to make comments in code that doesn't
+# run as actual code, and act as a way to  make notes in your code.
+
+
 
 # Open workshop with this video as context: https://www.youtube.com/watch?v=o0u4M6vppCI 
 
 # # Before you begin:
 # - review this content carefully and make any necessary modifications for yourself and your instructional style.
+# - though this guide documents the entire teaching process, be sure to stop for participant questions, ask important questions
+#   throughout as you see fit, and modify the story used as you deem fit for your audience.
 # - this is meant to be a guide to the workshop, but not an exact script. Take liberties with the story you create, and allow 
 #   for student input to guide the workshop, since it is created for their benefit.
 # - The code in this is chunked purposefully to be run together. Be sure to comment out anything you don't want to deal with
@@ -33,16 +38,18 @@
 
 ######################## Printing a String ########################
 
+# To begin a program, you have to type something. All great programers start somewhere, so we're going to start with "Hello".
 # In order to see the results of any program, you often need to print your output. Click inside of the type box loaded with the new 
 # program.
 # Achieve your first line of code with:
 print("Hello World")
-
+# Type this exactly. Use double quotes.
 # Run the program with the small green triangle. 
 
 # In this statement, the function 'print()' is passing the literal string "Hello World".
 # In programming, a sentence that you would see printed out is considered a string, which is a fancy
 # way of saying a line of characters.
+# A function does exactly as it says - it creates something in code that "functions." It gets SOMETHING done.
 # When you run the program, you will get 
 "Hello World"
 
@@ -60,7 +67,7 @@ My name is Alexander Hamilton
 And there's a million things I haven't done
 But just you wait, just you wait""")
 
-# You can use print(<parameter>) to print a number of things for a program.
+# You can use print(<parameter>) function to print a number of things for a program.
 # They don't necessarily have to be words, they can be any character you can type out on a keyboard.
 # However, whatever you print in the quotes, is exactly what you get out of the quotes. Examples:
 print("Gibb3rish")
@@ -68,6 +75,11 @@ print("3+2")
 # print('you're walking in the woods...&&%$#')
 
 # the last print statement doesn't work, and we got an error - can you tell me why?
+# (After answer received) Yes - if we look at the error message, there is a problem with syntax, which means how something
+# was typed. The error tells you where you are wrong and why it is wrong. 
+# We can see that the apostrophe makes an argument, and the computer is confused for it.
+# So we fix it by using double quotes, so that it does not think that the print statement ended halfway through, and 
+# we have useless junk in our code.
 print("you're walking in the woods...&&%$#")
 
 
@@ -80,7 +92,8 @@ print("you're walking in the woods...&&%$#")
 x = 3
 print(x)
 
-# You can do the same with a string. If you have a super long string (or short string you need >1), you can make it equal a variable.
+# You can do the same with a string. If you have a super long string (or short string you need more than once), 
+# you can make it equal a variable.
 inthewoods = """You’re walking in the woods, there is no one around, and your phone is dead. Out of the corner of your eye 
 you spot him. Its:"""
 
@@ -93,7 +106,8 @@ print(inthewoods)
 # Notice that it prints the content of inthewoods, but not the variable name "inthewoods". 
 # This is because you're essentially plug and chugging - you're plugging in a value for the variable, like plugging in the
 # coordinates to find the slope of a line (y=mx+b).
-# Python does the work for you, because it already knows that inthewoods equals your string.
+# Python does the work for you, because it already knows that inthewoods equals your string, so it just prints the string 
+# you want via the variable.
 
 ##### Prompt for questions so far #####
 
@@ -101,14 +115,15 @@ print(inthewoods)
 
 ######################## Prompt for input ########################
 
-# This will be important to our story!
+# Let's start a story where you can make a choice about what happens next.
+# We'll make a few variables, and equate them to the strings we want to print.
 inthewoods = """You’re walking in the woods, there is no one around, and your phone is dead. Out of the corner of your eye 
 you spot him. Its:"""
 choiceA = "A. Shia Labeouf"
 choiceB = "B. An adorable cat"
 
-# When you make a program, you may want information from the person who is using the program.
-# For our purposes, we want to get the user prompts to know what kind of story they want.
+# When you make a program, you may want information from the person who is using the program, like this story.
+# For our purposes, we want to get the user prompts to know what kind of story they want - a weird or innocent one.
 # In a choose your own adventure story, you have options to make the story yours. 
 # Ours won't be as complex as the memorable books from childhood or Black Mirror: Bandersnatch, but will give simple prompts to choose
 # a different story for each choice.
@@ -120,8 +135,10 @@ print(choiceB)
 # That was a lot of typing, and there might be any easier way to do that, which we'll get to later.
 firstanswer = input("Who is it? Type choiceA or choiceB ")
 			
-# the input function allows for a user to answer whatever prompt is in the input with some sort of typing. 
-# We're prompting the user here to type in the exact terms, so that the program will recognize that we need one of those answers.
+# the input function allows for a user to answer whatever prompt is in the input with some sort of typing, and the 
+# program will respond based on what is typed by running the input somehow.
+# We're prompting the user here to type in the exact terms, so that the program will recognize that we need one of those 
+# answers.
 # Later, we'll talk about how to use that answer to move to the next prompt. 
 print(firstanswer)
 
@@ -151,8 +168,9 @@ answer0 = input("Your choice (a/b): ")
 
 # You may have talked about in math using if statements. 
 # Whether you did or did not doesn't provide much advantage, so no worries to those who didn't.
-# An if statement provides a way of having options in a program, where whatever input is received may then lead the program to make 
-# certain choices so as to continue in a specific direction. 
+# An if statement provides a way of having options in a program, where whatever input is received may then lead the program 
+# to make certain choices so as to continue in a specific direction. 
+
 # Take this example:
 input("type a whole number above 0: ")
 if x == 2: # we are asking a question about a variable, known as x.
@@ -180,17 +198,19 @@ print(inthewoods)
 answer0 = (input("Your choice (a/b): ")  
 	   
 if answer0 == "a": 
-# here, you are starting with the if statement - if the answer by the user is an a, being if answer0 == a, then the following occurs:
+# here, you are starting with the if statement - if the answer by the user is an a, being if answer0 == a, then the following 
+# occurs:
 	print(Shia1) # good thing we're avoiding a whole chunk of string right here! It would look gross.
 	answershia1 = input("What do you do? (a/b): ")
 elif answer0 == "b":
+# elif is just a fancy way of saying "else/if" because we don't just want to say "else" here, because we have another parameter
+# we want to ask about.
 	print(Meow1)
 	answermeow1 = input("what do you do? (a/b): ")
+
 	   
-# we use those two add ons - '.lower()' and '.strip()' to modify the text so that anything close to our prompted answer is what
-# we get.
-# To make the next step, the information has to be within the if statement, per an indent. This is so the code knows that everything
-# whatever follows the statements in the indent, has to run first before moving on to the next step.	   
+# To make the next step, the information has to be within the if statement, per an indent. This is so the code knows that 
+# everything whatever follows the statements in the indent, has to run first before moving on to the next step.	   
 # Prompt for questions so far
 
 	   
